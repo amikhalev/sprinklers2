df2abb934037bd8109fbcc117beb763b809e72c2
amikhalev <alexmikhalevalex@gmail.com>
2016-01-12 11:29:19 -0700

    Fixed react router history

--- app/scripts/containers/Routes.jsx
+++ app/scripts/containers/Routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Router, Route, browserHistory} from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 
 import App from './App.jsx';
 import ProgramsPage from './ProgramsPage.jsx';
 import SectionsPage from './SectionsPage.jsx';
 
 const routes = (
-  <Router history={browserHistory}>
-    <Route component={App} path='/'>
+  <Router history={createBrowserHistory()}>
+    <Route path='/' component={App}>
       <Route path='sections' component={SectionsPage}/>
       <Route path='programs' component={ProgramsPage}/>
     </Route>
